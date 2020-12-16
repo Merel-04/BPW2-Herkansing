70febfd986d2c6ee480ce770db2aa6581e820344
Merel <merelw@gmail.com>
2020-12-16 15:03:13 +0100

    Add Models and texture's. Add FPS

---
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraBehaviour : MonoBehaviour
+{
+	[SerializeField] private Transform playerBody;
+	[Space]
+	[SerializeField] private string mouseXInputName = default;
+	[SerializeField] private string mouseYInputName = default;
+	[SerializeField] private float mouseSensitivity = default;
+
+
+	private float mouseXAxisClamp = 90f;
+
+	private float xAxisClamp = default;
+
+	private void Awake()
+	{
+		LockCursor(true);
+		xAxisClamp = 0.0f;
+	}
+
+	private void Update()
+	{
+		RotateCamera();
+	}
+
+	private void RotateCamera()
+	{
+		float multiplier = mouseSensitivity * Time.deltaTime;
+		float mouseX = Input.GetAxisRaw(mouseXInputName) * multiplier;
+		float mouseY = Input.GetAxisRaw(mouseYInputName) * multiplier;
+
+		xAxisClamp += mouseY;
+
+		if (xAxisClamp > mouseXAxisClamp)
+		{
+			xAxisClamp = 90f;
+			mouseY = 0f;
+			ClampXAxisRotationToValue(270);
+		}
+		else if (xAxisClamp < -mouseXAxisClamp)
+		{
+			xAxisClamp = -90f;
+			mouseY = 0f;
+			ClampXAxisRotationToValue(90);
+		}
+
+		transform.Rotate(Vector3.left * mouseY);
+		playerBody.Rotate(Vector3.up * mouseX);
+	}
+
+	private void ClampXAxisRotationToValue(float value)
+	{
+		Vector3 eulerRotation = transform.eulerAngles;
+		eulerRotation.x = value;
+		transform.eulerAngles = eulerRotation;
+	}
+
+	private void LockCursor(bool lockState)
+	{
+		Cursor.lockState = lockState ? CursorLockMode.Locked : CursorLockMode.None;
+	}
+}
---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+	[SerializeField] private string horizontalInputName = default;
+	[SerializeField] private string verticalInputName = default;
+
+	private float movementSpeed = default;
+	[SerializeField] private float walkSpeed = default;
+	[SerializeField] private float runSpeed = default;
+	[SerializeField] private float runBuildUpSpeed = default;
+	[SerializeField] private KeyCode runKey = default;
+
+	[SerializeField] private float slopeForce = default;
+	[SerializeField] private float slopeForceRayLength = default;
+	[Space]
+	[SerializeField] private bool isJumping = false;
+	[SerializeField] private AnimationCurve jumpfallOff = default;
+	[SerializeField] private float jumpMultiplier = default;
+
+	[SerializeField] private KeyCode jumpKey = default;
+
+	private CharacterController charController = default;
+
+	private void Awake()
+	{
+		charController = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		PlayerMovement();
+	}
+
+	private void PlayerMovement()
+	{
+		float horInput = Input.GetAxisRaw(horizontalInputName);
+		float verInput = Input.GetAxisRaw(verticalInputName);
+
+		Vector3 forwardMovement = transform.forward * verInput;
+		Vector3 rightMovement = transform.right * horInput;
+
+		charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1f) * movementSpeed);
+
+		if ((verInput != 0f || horInput != 0f) && OnSlope())
+			charController.Move(Vector3.down * charController.height / 2f * slopeForce * Time.deltaTime);
+
+		SetMovementSpeed();
+		JumpInput();
+	}
+
+	private void SetMovementSpeed()
+	{
+		if (Input.GetKey(runKey))
+			movementSpeed = Mathf.Lerp(movementSpeed, runSpeed, Time.deltaTime * runBuildUpSpeed);
+		else
+			movementSpeed = Mathf.Lerp(movementSpeed, walkSpeed, Time.deltaTime * runBuildUpSpeed);
+	}
+
+	private void JumpInput()
+	{
+		if (Input.GetKeyDown(jumpKey) && !isJumping)
+		{
+			isJumping = true;
+			StartCoroutine(JumpEvent());
+		}
+	}
+
+	private IEnumerator JumpEvent()
+	{
+		charController.slopeLimit = 90f;
+		float timeInAir = 0f;
+		do
+		{
+			float jumpForce = jumpfallOff.Evaluate(timeInAir);
+			charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+			timeInAir += Time.deltaTime;
+			yield return null;
+		} while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+		charController.slopeLimit = 45f;
+		isJumping = false;
+	}
+
+	private bool OnSlope()
+	{
+		if (isJumping)
+			return false;
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2f * slopeForceRayLength))
+			if (hit.normal != Vector3.up)
+				return true;
+		return false;
+	}
+}
